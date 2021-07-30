18a20359ec40e408b03575fa2c7f7d03090e27f2
Yun-D <yund_272@naver.com>
2021-07-31 02:28:07 +0900

    ⚡ 카운터 코드 개선

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
- import { render } from '@testing-library/react';
-import {createStore} from 'redux';
- 
- const add = document.getElementById("plus"); 
- const minus = document.getElementById("minus"); 
- const number = document.querySelector("span");
+import { createStore } from 'redux';
 
- number.innerText = 0;
+const add = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+//오타 사고를 미리 방지하기 위해 모든 string을 변수화!
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 const countModifier = (count = 0, action) => { //데이터(state) 설정. 이 인자들은 현재의 state!!
   //인자(argument)로 default state를 Initializing할 수 있다.
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS"){
-    return count - 1;
-  } else
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 }
 const countStore = createStore(countModifier); //스토어 생성
 
@@ -24,8 +29,8 @@ const onChange = () => { //store에 변화가 있을 때 마다 감지해서 불
 countStore.subscribe(onChange);
 
 add.addEventListener("click", () => {
-  countStore.dispatch({type: "ADD"}); //action 보내기
+  countStore.dispatch({ type: ADD }); //action 보내기
 })
 minus.addEventListener("click", () => {
-  countStore.dispatch({type: "MINUS"});
+  countStore.dispatch({ type: MINUS });
 })
