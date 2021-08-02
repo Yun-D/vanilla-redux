dcb01a72b28e0ce433d63ddd4f334aa6e2351eeb
Yun-D <yund_272@naver.com>
2021-08-03 03:08:43 +0900

    ✅  toDo _vanilla JS

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
-import { createStore } from 'redux';
 
-const add = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
 
-number.innerText = 0;
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-//오타 사고를 미리 방지하기 위해 모든 string을 변수화!
-const ADD = "ADD";
-const MINUS = "MINUS";
+const toDOs = [];
 
-const countModifier = (count = 0, action) => { //데이터(state) 설정. 이 인자들은 현재의 state!!
-  //인자(argument)로 default state를 Initializing할 수 있다.
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
+const createTodo = toDo => { //얻은 값을 인자로 
+  const li = document.createElement("li"); //리스트 아이템 만들고
+  li.innerText = toDo; //받은 값을 아이템 텍스트로 바꿔줌
+  ul.appendChild(li);
 }
-const countStore = createStore(countModifier); //스토어 생성
 
-const onChange = () => { //store에 변화가 있을 때 마다 감지해서 불려질 함수!
-  number.innerText = countStore.getState(); //html에게 뭔가 바뀌었다고 알려주기 위해 사용
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value; //inputText에서 값 가져오기!
+  input.value = "";
+  createTodo(toDo); //함수 호출
 }
-countStore.subscribe(onChange);
 
-add.addEventListener("click", () => {
-  countStore.dispatch({ type: ADD }); //action 보내기
-})
-minus.addEventListener("click", () => {
-  countStore.dispatch({ type: MINUS });
-})
+form.addEventListener("submit", onSubmit); //submit 됐을 경우 input에서 값 가져옴
