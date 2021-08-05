c8cf7211a8b4e277b7581b7c71a7a4dec3000d02
Yun-D <yund_272@naver.com>
2021-08-06 02:12:08 +0900

    ⚡redux 도입중...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import { Provider } from 'react-redux';
 import App from "./components/App";
+import store from './routes/store';
 
-reactDom.render(<App></App>, document.getElementById("root")); 
+reactDom.render(
+  <Provider store={store}>
+    {/* index와 store를 연결 */}
+    <App />
+  </Provider>, 
+  document.getElementById("root")); 
---
+++ src/routes/store.js
@@ -0,0 +1,37 @@
+import {createStore} from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+const addToDO = (text) => {
+    return {
+        type: ADD,
+        text
+    }
+}
+
+const deleteToDO = (id) => {
+    return {
+        type: DELETE,
+        id
+    }
+}
+
+const reducer = (state = [], action) => {
+    switch (action.type) {
+        case ADD: {
+            return [{ text: action.text, id: Date.now()}, ...state];
+        }
+        case DELETE: {
+            return state.filter(toDo => toDo.id !== action.id);
+        }
+        default: 
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+store.subscribe(); //변동 사항이 생기면 모든 것을 rerender 하도록 react-redux를 사용할 것
+
+export default store;
